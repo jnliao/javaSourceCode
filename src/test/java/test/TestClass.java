2adf4bcda87246cfdc8bde74570fc9a7bf90ba27
liaojinneng <jinneng.liao@zhangmen.com>
2021-08-21 11:04:32 +0800

    搭建jdk源码阅读环境

---
+++ src/test/java/test/TestClass.java
@@ -0,0 +1,17 @@
+package test;
+
+/**
+ * @author jinneng.liao
+ */
+public class TestClass {
+
+    public static void main(String[] args){
+        testInteger();
+    }
+
+    public static void testInteger(){
+        Integer a = 1;
+        System.out.println("testInteger:"+a);
+    }
+
+}
